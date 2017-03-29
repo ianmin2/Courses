2dd716e51842e5b1b942f9d9ace4a2672dd0c60f
Ian Innocent Mbae <ianmin2@live.com>
2017-03-29 14:04:48 +0300

    Update courses.sql

--- sql/courses.sql
+++ sql/courses.sql
@@ -1,11 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.0.9
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Jun 05, 2014 at 09:53 AM
--- Server version: 5.6.14
--- PHP Version: 5.5.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -15,10 +7,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-
---
--- Database: `courses`
---
 
 -- --------------------------------------------------------
 
@@ -41,8 +29,35 @@ CREATE TABLE IF NOT EXISTS `country` (
 CREATE TABLE IF NOT EXISTS `courses` (
   `id` bigint(255) NOT NULL AUTO_INCREMENT,
   `cour_name` varchar(100) NOT NULL,
-  `cour_major` bigint(255) NOT NULL,
+  `cour_dept`  bigint(255) NOT NULL,
+  `cour_code`  varchar(25) NOT NULL,
+  `cour_weight` bigint(1),
+  `cour_major` bigint(255),
   `cour_inst` bigint(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `grades`
+--
+
+CREATE TABLE IF NOT EXISTS `grades` (
+  `id` bigint(255) NOT NULL AUTO_INCREMENT,
+  `ap` smallint(3) NOT NULL,
+  `a` smallint(3) NOT NULL,  
+  `am` smallint(3) NOT NULL,
+  `bp` smallint(3) NOT NULL,
+  `b` smallint(3) NOT NULL,
+  `bm` smallint(3) NOT NULL,
+  `cp` smallint(3) NOT NULL,
+  `c` smallint(3) NOT NULL,
+  `cm` smallint(3) NOT NULL,
+  `dp` smallint(3) NOT NULL,
+  `d` smallint(3) NOT NULL,
+  `dm` smallint(3) NOT NULL,
+  `grades_inst` bigint(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
